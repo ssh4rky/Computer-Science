1981e9027533e93ee5dcf82fb844588bcef99375
ssh4rky <sigmovic1488@gmail.com>
2025-11-05 12:51:09 +0200

    Add project files.

---
+++ ConsoleApplication147/ConsoleApplication147.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(65001);
+	SetConsoleOutputCP(65001);
+
+	cout << u8"Перебрати будь-яку з колекцій данних, вивести елементи в окремий файл, файл створити у середовищі" << endl;
+}
